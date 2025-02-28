c541c0b95d5faf84a28ff09be9a36bc51cd83fff
PAPASIN <joseomar_coppola@hotmail.com>
2025-02-28 12:07:13 +0100

    28-02-2025, adding just one index.ts for classes 101

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,69 @@
-let userAge: number = 27;
-let firstName: string = "Omar";
-let isFictional: boolean;
+/* classes 101  */
 
-userAge = 28;
-firstName = "Jose";
-isFictional = false;
+type Base = "classic" | "thick" | "thin" | "garlic";
 
-//it can also
-let planet = "Earth";
-let moons = 1;
-let isLarge = false;
+interface HasFormatter {
+  format(): string;
+}
 
-planet = "Mars";
-moons = 2;
-isLarge = true;
+/* Inheritance && abstract class */
+abstract class MenuItem implements HasFormatter {
+  constructor(private title: string, private price: number) {}
 
-//null & undefined
-let something: null;
-let anotherThing: undefined;
+  get details(): string {
+    return `${this.title} - $${this.price}`;
+  }
+
+  format() {
+    return `This menu item is called ${this.title} and is $${this.price}`;
+  }
+}
+
+class Pizza extends MenuItem {
+  constructor(title: string, price: number) {
+    //using the super to have the parameters from the menuItem
+    super(title, price);
+  }
+
+  private base: Base = "classic";
+  private toppings: string[] = [];
+
+  addTopping(topping: string): void {
+    this.toppings.push(topping);
+  }
+
+  removeTopping(topping: string): void {
+    this.toppings = this.toppings.filter((t) => t !== topping);
+  }
+
+  selectBase(b: Base): void {
+    this.base = b;
+  }
+}
+
+const pizzaOne = new Pizza("omar special", 27);
+console.log(pizzaOne);
+
+/* classes as Types */
+function addMushroomsToPizzas(pizzas: Pizza[]): void {
+  for (const p of pizzas) {
+    p.addTopping("mushrooms");
+  }
+}
+
+/* addMushroomsToPizzas([pizzaOne]);
+console.log(pizzaOne); */
+
+function printMenuItem(item: MenuItem): void {
+  console.log(item.details);
+}
+
+console.log(pizzaOne.details);
+
+function printFormatted(val: HasFormatter): void {
+  console.log(val.format());
+}
+
+printFormatted(pizzaOne);
+
+/* abstract class */
