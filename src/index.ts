39551d3345210edd1bdb2872c3e99c91c0056a4e
PAPASIN <joseomar_coppola@hotmail.com>
2025-03-04 09:49:03 +0100

    04-03-2025, adding to the typoscripto repo generics theory, refactoring the csv write miniproject

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,9 @@
 
 import { appendFileSync } from "fs";
 
-interface Payment {
-  to: string;
-  id: number;
-  amount: number;
-  notes: string;
-}
-
-type PaymentColums = ("id" | "amount" | "to" | "notes")[];
-
-class CSVWriter {
-  constructor(private columns: PaymentColums) {
-    this.csv = this.columns.join("");
+export class CSVWriter<T> {
+  constructor(private columns: (keyof T)[]) {
+    this.csv = this.columns.join(",") + "\n";
   }
 
   private csv: string;
@@ -24,24 +15,14 @@ class CSVWriter {
     console.log(`file saved as ${filename}`);
   }
 
-  addRows(values: Payment[]): void {
+  addRows(values: T[]): void {
     let rows = values.map((v) => this.formateRow(v));
     this.csv += rows.join("\n");
 
     console.log(this.csv);
   }
 
-  private formateRow(p: Payment): string {
-    return this.columns.map((col) => p[col]).join(",");
+  private formateRow(value: T): string {
+    return this.columns.map((col) => value[col]).join(",");
   }
 }
-
-const writer = new CSVWriter(["id", "amount", "to", "notes"]);
-
-writer.addRows([
-  { id: 1, amount: 100, to: "Sarah", notes: "Lunch" },
-  { id: 2, amount: 200, to: "John", notes: "Dinner" },
-  { id: 3, amount: 300, to: "Jane", notes: "Breakfast" },
-]);
-
-writer.save("./data/payments.csv");
